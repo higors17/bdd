f735cf365d10b4f931baed4ad25e343b62dc6902
Higor <hsilvapereira1@gmail.com>
2026-06-12 20:26:56 -0300

    atualização
    relações

--- aula-locadora.sql
+++ aula-locadora.sql
@@ -26,3 +26,46 @@ CREATE TABLE Diretores (
         REFERENCES Filmes(Id),
     FOREIGN KEY ( IdGenero )
         REFERENCES Generos(Id) );
+
+    CREATE TABLE Clientes (
+    Id INT PRIMARY KEY IDENTITY,
+    Nome VARCHAR (200),
+    Email VARCHAR (100)
+    );
+
+    CREATE TABLE Locacoes (
+    Id INT PRIMARY KEY IDENTITY,
+    IdCliente INT,
+    IdFilme INT,
+    DataLocacao DATE,
+    FOREIGN KEY ( IdCliente )
+       REFERENCES Clientes (Id),
+    FOREIGN KEY ( IdFilme )
+       REFERENCES Filmes (Id),
+
+    INSERT INTO Generos (Nome)
+        VALUES 
+       ('Ação'),
+       ('Drama'),
+       ('Ficção Científica');
+
+       INSERT INTO Diretores (Nome)
+        VALUES 
+       ('Christopher Nolan'),
+       ('Quentin Tarantino');
+
+        INSERT INTO Filmes (Nome, Ano, IdDiretor)
+        VALUES 
+       ('A origem', 2010, 1),
+       ('Django Livre', 2012, 2),
+       ('Interestelar', 2014, 1);
+
+        INSERT INTO FilmesGeneros (IdFilme, IdGenero)
+        VALUES (1, 1),
+       (1, 3),
+       (2, 2),
+       (3, 2),
+       (3, 3);
+
+       SELECT * FROM Filmes;
+   
